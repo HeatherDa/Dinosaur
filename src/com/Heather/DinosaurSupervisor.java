409ac466bcc76c2fde8481cee9f6932a2439610c
heather <crystal_tree@hotmail.com>
2016-02-18 18:05:30 -0600

    Started Dinosaur program

---
+++ src/com/Heather/DinosaurSupervisor.java
@@ -0,0 +1,13 @@
+package com.Heather;
+
+public class DinosaurSupervisor {
+
+    public static void main(String[] args) {
+	// write your code here
+        Dinosaur t_rex=new Dinosaur();
+        Dinosaur velociraptor=new Dinosaur();
+
+        t_rex.roar();
+        velociraptor.roar();
+    }
+}
